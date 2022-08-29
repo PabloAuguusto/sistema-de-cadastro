d0175b796b5aad09af73d16b4d8070792b5bc219
pablo <pabloauguusto@hotmail.com>
2022-08-29 15:21:50 -0300

    voltando para o estado do hiruko

--- app.js
+++ app.js
@@ -2,4 +2,3 @@ console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("aplicação está se conectando ao banco de dados")
 // mais um console log teste:
 console.log("O hiruko fez esse commit sem voce ver na aula")
-console.log("enviando para o GitHub")
