16b404fa6a61e41fd3134fa986261a9c1580914e
PabloAuguusto <104510137+PabloAuguusto@users.noreply.github.com>
2022-08-29 15:03:59 -0300

    o hiruko fez alteração no codigo sem voce ver na aula

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
+console.log("aplicação está se conectando ao banco de dados")
+// mais um console log teste:
+console.log("O hiruko fez esse commit sem voce ver na aula")
